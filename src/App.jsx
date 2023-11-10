687e63e8ddcf9d32b27519e76d206c5de5b3d7fd
Tom Serenander <tom@serenander.se>
2023-11-10 09:43:08 +0100

    Read and write notes from/to localStorage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Sidebar from './components/Sidebar'
 import Editor from './components/Editor'
 import { data } from '../data.js'
 import Split from 'react-split'
 import { nanoid } from 'nanoid'
 
-/**
- * Challenge: Spend 10-20+ minutes reading through the code
- * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you
- * understand the existing code (although you don't need
- * to fully understand everything to move on)
- */
-
 export default function App() {
-	const [notes, setNotes] = useState([])
+	const [notes, setNotes] = useState(
+		() => JSON.parse(localStorage.getItem('notes')) || []
+	)
 	const [currentNoteId, setCurrentNoteId] = useState(
 		(notes[0] && notes[0].id) || ''
+	)
+
+	useEffect(
+		() => localStorage.setItem('notes', JSON.stringify(notes)),
+		[notes]
 	)
 
 	function createNewNote() {
